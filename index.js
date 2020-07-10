0e4c54a972010f02fb55b97e8f1ad1e6bd9878d8
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-10 16:02:27 -0700

    initial server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/', (req, res) => res.send("HELLO WORLD!"));
+
+app.get('/logs', (req, res) => {
+    console.log(req.body)
+})
