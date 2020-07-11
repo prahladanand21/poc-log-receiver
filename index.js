411c56ffc6afa5d20f76bd6836d87f2483e35072
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-10 17:36:48 -0700

    added body parser

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-
+app.use(bodyParser.text({"type": "application/logplex-1"}))
 app.get('/', (req, res) => res.send("HELLO WORLD!"));
 
 app.post('/logs', (req, res) => {
-    console.log(req.body)
-    console.log(req.headers)
+    console.log(req.body);
+    console.log(req.headers);
+    res.setHeader("Content-Length", 0)
     return res.sendStatus(200);
 })
 
