4bddce0e7e2057e197e903a0587a4ac4a5d00bb4
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-16 12:46:41 -0700

    added syslog parsing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express');
+const parse = require('syslog-parse');
+const bodyParser = require('body-parser');
+const logCache = require('./src/models/logCache')
 const app = express()
 const port = 3000
 
-var logs = []
+const logs = []
 var cache = []
 
 app.use(bodyParser.text({"type": "application/logplex-1"}))
@@ -21,7 +23,10 @@ setInterval(getLogsPerMinute, 60000);
 app.post('/logs', (req, res) => {
     console.log(req.body);
     const currLogs = req.body.split('\n');
-    cache.push(...currLogs);
+    currLogs.forEach(log => {
+      logCache.addLog(log);  
+    });
+    logCache.print();
     res.setHeader("Content-Length", 0)
     return res.sendStatus(200);
 })
---
+++ src/models/logCache.js
@@ -0,0 +1,31 @@
+const parse = require('syslog-parse')
+class LogCache {
+    constructor () {
+        this.cache = []
+    }
+
+    /**
+     * Add parsed log entry to cache
+     * @param {Object} logEntry The parsed log entry
+     */
+    addLogEntry (logEntry) {
+        this.cache.push(logEntry);
+    }
+
+    /**
+     * Parse log message string from logplex and add to cache
+     * @param {String} log The syslog message sent from logplex 
+     */
+    addLog (log) {
+        const logEntry = parse(log);
+        this.addLogEntry(logEntry)
+    }
+
+    print() {
+        console.log(JSON.stringify(this.cache, null, 4));
+    }
+
+}
+
+
+module.exports = new LogCache();
