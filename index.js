1bedcfb974a5dea61dac6b1f9b57c82293b17d9f
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-16 12:07:19 -0700

    added setInterval to getlogsperminute

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+var logs = []
+var cache = []
+
 app.use(bodyParser.text({"type": "application/logplex-1"}))
 app.get('/', (req, res) => res.send("HELLO WORLD!"));
 
+function getLogsPerMinute() {
+    logs.push(...cache);
+    console.log("Number of logs in last minute: " + cache.length);
+    console.log("Total Number of logs: " + logs.length);
+    cache = [];
+}
+
+setInterval(getLogsPerMinute, 60000);
+
 app.post('/logs', (req, res) => {
     console.log(req.body);
-    console.log(req.headers);
+    const currLogs = req.body.split('\n');
+    cache.push(...currLogs);
     res.setHeader("Content-Length", 0)
     return res.sendStatus(200);
 })
+
 
 app.listen((process.env.PORT || port), () => {
     console.log("listening....")
