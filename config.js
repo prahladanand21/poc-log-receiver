a964cef2285aa362a9906ca81c2efe6a423bef38
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-28 16:59:47 -0700

    add 443 port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ajnaURL: 'https://ajna-api.salesforce.com/v1/metrics/v2',
+    ajnaURL: 'https://ajna-api.salesforce.com:443/v1/metrics/v2',
     ajnaKey:  process.env.AJNA_KEY || undefined,
     ajnaCert:  process.env.AJNA_CERT || undefined,
 }
