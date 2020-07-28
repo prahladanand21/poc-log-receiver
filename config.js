9444db7e8b4083ed2944ce7ca29c1fefdc8b1f03
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-28 16:44:16 -0700

    log res and fix config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ajnaURL = 'https://ajna-api.salesforce.com/v1/metrics/v2',
-    ajnaKey = process.env.AJNA_KEY || undefined,
-    ajnaCert = process.env.AJNA_CERT || undefined,
+    ajnaURL: 'https://ajna-api.salesforce.com/v1/metrics/v2',
+    ajnaKey:  process.env.AJNA_KEY || undefined,
+    ajnaCert:  process.env.AJNA_CERT || undefined,
 }
