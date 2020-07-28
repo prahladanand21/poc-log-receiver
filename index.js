26d3931a8c960559b0770c540656daa38b744f09
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-28 16:40:11 -0700

    added key and cert + ajna request

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const logCache = require('./src/models/logCache')
-const app = express()
-const moment = require('moment')
-const port = 3000
+const logCache = require('./src/models/logCache');
+const moment = require('moment');
+const config = require('./config');
+const request = require('superagent');
 
-app.use(bodyParser.text({"type": "application/logplex-1"}))
-app.get('/', (req, res) => res.send("HELLO WORLD!"));
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.text({ type: 'application/logplex-1' }));
+app.get('/', (req, res) => res.send('HELLO WORLD!'));
 
 function getLogsPerMinute() {
-    const logs = logCache.getLogs()
-    console.log(`Timestamp: ${moment.utc()}`)
-    console.log("Number of logs in last minute: " + logs.length);
+    const logs = logCache.getLogs();
+    const metrics = logCache.getMetrics(logs);
+    console.log(`Timestamp: ${moment.utc()}`);
+    console.log('Number of logs in last minute: ' + logs.length);
     console.log(`Average Request Rate: ${logs.length / 60} logs/sec`);
+
+    request
+        .post(config.ajnaURL)
+        .key(config.ajnaKey)
+        .cert(config.ajnaCert)
+        .set('Content-Type', 'application/json')
+        .send(metrics)
+        .then((res) => {
+            console.log('Metrics sent successfully');
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+        
     logCache.clear();
 }
 
 app.post('/logs', (req, res) => {
     console.log(req.body);
     logCache.addLog(req.body);
-    res.setHeader("Content-Length", 0)
+    res.setHeader('Content-Length', 0);
     return res.sendStatus(200);
-})
+});
 
-
-app.listen((process.env.PORT || port), () => {
-    console.log("listening....")
+app.listen(process.env.PORT || port, () => {
+    console.log('listening....');
     // Putting this inside listen to avoid loss of logs during start up
     setInterval(getLogsPerMinute, 60000);
-})
+});
--- src/models/logCache.js
+++ src/models/logCache.js
@@ -1,8 +1,8 @@
-const parser = require('heroku-log-parser')
-const moment = require('moment')
+const parser = require('heroku-log-parser');
+const moment = require('moment');
 
 class LogCache {
-    constructor () {
+    constructor() {
         this.cache = [];
     }
 
@@ -10,34 +10,34 @@ class LogCache {
      * Add parsed log entry to cache
      * @param {Object} logEntry The parsed log entry
      */
-    addLogEntry (logEntry) {
+    addLogEntry(logEntry) {
         this.cache.push(logEntry);
     }
 
     /**
      * Parse log message string from logplex and add to cache
-     * @param {String} log The syslog message sent from logplex 
+     * @param {String} log The syslog message sent from logplex
      */
-    addLog (log) {
+    addLog(log) {
         const logEntries = parser.parse(log);
-        logEntries.forEach(entry => {
+        logEntries.forEach((entry) => {
             const record = {
-                'timestamp': moment(entry['emitted_at']).valueOf(),
-                'message': entry['message']
+                timestamp: moment(entry['emitted_at']).valueOf(),
+                message: entry['message'],
             };
             this.addLogEntry(record);
-        })
+        });
     }
 
     /**
-     * 
-     * @param {String} curr_time 
+     *
+     * @param {String} curr_time
      */
     getLogs() {
-        const currentTime = moment.utc()
+        const currentTime = moment.utc();
         const startTime = currentTime - 60000;
         const result = [];
-        this.cache.forEach(log => {
+        this.cache.forEach((log) => {
             if (log['timestamp'] <= currentTime && log['timestamp'] >= startTime) {
                 result.push(log);
             }
@@ -54,12 +54,27 @@ class LogCache {
     }
 
     /**
-     * Clear short term cache and move contents to long term store 
+     * Clear short term cache and move contents to long term store
      */
     clear() {
         this.cache = [];
     }
-}
 
+    getMetrics(logs) {
+        const metrics = [];
+
+        // Generate and Add Rate Metric
+        const requestRate = logs.length / 60;
+        const rate = {
+            'service': 'trustTest',
+            'metricName': ['Rate'],
+            'metricValue': requestRate,
+            'timestamp': moment.utc().valueOf(),
+        };
+        metrics.push(rate);
+
+        return metrics;
+    }
+}
 
 module.exports = new LogCache();
